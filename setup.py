3d7088cde36d0fd6b451788eeef08f5e1825b172
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-06-16 22:59:27 +0100

    Added Alex to the authors list in setup.py.

--- setup.py
+++ setup.py
@@ -30,12 +30,11 @@ from distutils.core import setup
 setup(name='git-rdm',
       version='1.0',
       description='Git-RDM is a research data management plugin for the Git version control system.',
-      author='Christian T. Jacobs',
+      author='Christian T. Jacobs, Alexandros Avdis',
       author_email='christian@christianjacobs.uk',
       url='https://github.com/ctjacobs/git-rdm',
       scripts=["git-rdm"],
       classifiers=[
-          'Development Status :: 4 - Beta',
           'Environment :: Console',
           'Intended Audience :: Science/Research',
           'Natural Language :: English',
